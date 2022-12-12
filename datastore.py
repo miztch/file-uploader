2b88209f83b6356d9be2307eccf2394885448b2c
miztch <miztch.me@gmail.com>
2022-12-13 03:14:41 +0900

    pass boto3 region name

--- datastore.py
+++ datastore.py
@@ -1,7 +1,9 @@
+import os
+
 import boto3
 
-s3 = boto3.client('s3')
-dynamodb = boto3.resource('dynamodb')
+s3 = boto3.client('s3', region_name=os.environ['AWS_REGION'])
+dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
 
 
 def s3_put_object(bucket_name, upload_data, object_key):
